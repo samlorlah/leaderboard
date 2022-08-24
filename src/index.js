366f679d68042d430becebf4c43956aec432f656
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-08-24 08:18:46 +0100

    Add already generated gameID to replace dynamic generation by user
    Blocked user from submitting empty string for name and score

--- src/index.js
+++ src/index.js
@@ -5,33 +5,35 @@ const submitBtn = document.querySelector('.submit_btn');
 const refreshBtn = document.querySelector('.refresh_btn');
 
 const data = new Data();
-data.setgameId();
 data.displayData();
 
 submitBtn.addEventListener('click', async () => {
   const user = document.getElementById('name_field');
   const score = document.getElementById('score_field');
-  const formData = {
-    user: user.value,
-    score: score.value,
-  };
 
-  const url = `${data.baseUrl}games/${data.gameId}/scores`;
+  if (user.value !== '' && score.value !== '') {
+    const formData = {
+      user: user.value,
+      score: score.value,
+    };
 
-  await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data.scores = data.result;
-    });
+    const url = `${data.baseUrl}games/${data.gameId}/scores`;
 
-  user.value = '';
-  score.value = '';
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.scores = data.result;
+      });
+
+    user.value = '';
+    score.value = '';
+  }
 });
 
 refreshBtn.addEventListener('click', () => {
--- src/scoreData.js
+++ src/scoreData.js
@@ -2,29 +2,8 @@ const tableBody = document.querySelector('.score_data');
 class Data {
   constructor() {
     this.scores = [];
-    this.gameId = localStorage.getItem('gameId')
-      ? localStorage.getItem('gameId')
-      : '';
+    this.gameId = 'nllsFxXRaSMyHqrZt0w1';
     this.baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-  }
-
-  async setgameId() {
-    if (!this.gameId) {
-      const gameName = { name: 'SamlorlahGame' };
-      const url = `${this.baseUrl}games`;
-      await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(gameName),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          const gameCode = data.result.split('ID: ')[1].split(' ')[0];
-          localStorage.setItem('gameId', gameCode);
-        });
-    }
   }
 
   async displayData() {
